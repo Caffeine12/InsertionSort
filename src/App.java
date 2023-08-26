265549b04b43d0ad7ded0defd90192d10cfb911f
Caffeine12 <jubairku12@gmail.com>
2023-08-27 01:43:24 +0200

    Algorithm corrected-Successful-27 Aug 2023

--- src/App.java
+++ src/App.java
@@ -4,15 +4,15 @@ public class App {
     public static void main(String[] args) throws Exception {
         int arr[]={10, 8, 12, 14, 6, 16};
         int swap=0;
-        for(int i=1;i<=arr.length;i++)
+        for(int i=1;i<=arr.length-1;i++)
         {
-            for(int j=0;j<arr.length-1;j++)
+            for(int j=i;j>=1;j--)
             {
-                if(arr[j]>arr[j+1])
+                if(arr[j]<arr[j-1])
                 {
                     swap=arr[j];
-                    arr[j]=arr[j+1];
-                    arr[j+1]=swap;
+                    arr[j]=arr[j-1];
+                    arr[j-1]=swap;
                 }
             }
         }
